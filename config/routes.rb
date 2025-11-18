32876313b637de122c406206e554afdf53e5a345
Karen Barbé <info@karenbarbe.com>
2025-11-18 12:37:28 -0600

    create custom routes for login and signup and restrict users and session resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  resources :users
-  resources :stash_items
-  resource :session
+  get "/dashboard" => "pages#dashboard", as: :dashboard
+
+  # Custom sign in and sign out routes
+  get "/login" => "sessions#new", as: :login
+  post "/login" => "sessions#create"
+
+  get "/signup" => "users#new", as: :signup
+
+  resources :users, only: [ :new, :create, :show ]
+  resource :session, only: [ :new, :create, :destroy ]
   resources :passwords, param: :token
+
   resources :product_colors
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  # get("/your_first_screen", { :controller => "pages", :action => "first" })
+  resources :stash_items
+
 end
