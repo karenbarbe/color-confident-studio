dd9e9bd83a86343d84d439ed82289556f926a675
Karen Barbé <info@karenbarbe.com>
2025-11-18 13:38:06 -0600

    add OKLCH columns to ProductColors

--- app/models/product_color.rb
+++ app/models/product_color.rb
@@ -5,6 +5,9 @@
 #  id          :bigint           not null, primary key
 #  hex_color   :string
 #  name        :string
+#  oklch_c     :decimal(4, 3)
+#  oklch_h     :decimal(6, 3)
+#  oklch_l     :decimal(4, 3)
 #  vendor_code :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
@@ -13,6 +16,9 @@
 # Indexes
 #
 #  index_product_colors_on_brand_id  (brand_id)
+#  index_product_colors_on_oklch_c   (oklch_c)
+#  index_product_colors_on_oklch_h   (oklch_h)
+#  index_product_colors_on_oklch_l   (oklch_l)
 #
 # Foreign Keys
 #
--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_18_031100) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_18_185444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -27,7 +27,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_18_031100) do
     t.string "hex_color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "oklch_l", precision: 4, scale: 3
+    t.decimal "oklch_c", precision: 4, scale: 3
+    t.decimal "oklch_h", precision: 6, scale: 3
     t.index ["brand_id"], name: "index_product_colors_on_brand_id"
+    t.index ["oklch_c"], name: "index_product_colors_on_oklch_c"
+    t.index ["oklch_h"], name: "index_product_colors_on_oklch_h"
+    t.index ["oklch_l"], name: "index_product_colors_on_oklch_l"
   end
 
   create_table "sessions", force: :cascade do |t|
