f349702af7455bb5c7136d240a2b87966f76b7fe
Karen Barbé <info@karenbarbe.com>
2025-11-21 14:49:25 -0600

    add category column to brand and enum

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,6 +3,7 @@
 # Table name: brands
 #
 #  id                   :bigint           not null, primary key
+#  category             :string           default("general")
 #  name                 :string
 #  product_colors_count :integer          default(0), not null
 #  slug                 :string
@@ -11,10 +12,13 @@
 #
 # Indexes
 #
-#  index_brands_on_slug  (slug) UNIQUE
+#  index_brands_on_category  (category)
+#  index_brands_on_slug      (slug) UNIQUE
 #
 class Brand < ApplicationRecord
   has_many :product_colors, dependent: :destroy
+
+  enum :category, { general: "general", fabric: "fabric", thread: "thread" }
 
   before_validation :generate_slug
   validates :slug, presence: true, uniqueness: true
--- db/cache_schema.rb
+++ db/cache_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_21_200452) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_21_202857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_21_200452) do
     t.datetime "updated_at", null: false
     t.integer "product_colors_count", default: 0, null: false
     t.string "slug"
+    t.string "category", default: "general"
+    t.index ["category"], name: "index_brands_on_category"
     t.index ["slug"], name: "index_brands_on_slug", unique: true
   end
 
