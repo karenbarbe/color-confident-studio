3780bc2bda05020c5ab9d3c37a7a38fd6976aa7c
Karen Barbé <info@karenbarbe.com>
2025-11-25 21:46:54 -0600

    add featured brand and stashed color IDs to color libraries controller

--- app/controllers/color_libraries_controller.rb
+++ app/controllers/color_libraries_controller.rb
@@ -3,6 +3,9 @@ class ColorLibrariesController < ApplicationController
   def index
     @categories = Brand.categories.keys
     @brands_by_category = Brand.all.group_by(&:category)
+    @featured = Brand.find_by(name: "DMC")
+    @featured_colors = @featured.product_colors.order(:id)
+    @stashed_color_ids = Current.user.stash_items.pluck(:product_color_id).to_set
   end
 
   def category
@@ -17,5 +20,6 @@ class ColorLibrariesController < ApplicationController
     @product_colors = @brand.product_colors.order(:id)
     @all_categories = Brand.categories.keys
     @brands_in_category = Brand.where(category: @category).order(:name)
+    @stashed_color_ids = Current.user.stash_items.pluck(:product_color_id).to_set
   end
 end
