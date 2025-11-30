23e746a1959e52c649a015f8c4f50ab25a011025
Karen Barbé <info@karenbarbe.com>
2025-11-29 19:27:22 -0600

    refactor stashed color retrieval to filter by brand and count stashed colors

--- app/controllers/color_libraries_controller.rb
+++ app/controllers/color_libraries_controller.rb
@@ -18,7 +18,13 @@ class ColorLibrariesController < ApplicationController
     @category = params[:category]
     @brand = Brand.find_by!(slug: params[:brand_slug], category: @category)
     @product_colors = @brand.product_colors.order(:id)
-    @stashed_color_ids = Current.user.stash_items.pluck(:product_color_id).to_set
+    @stashed_color_ids = Current.user.stash_items
+                            .joins(:brand)
+                            .where(brands: { id: @brand.id })
+                            .pluck(:product_color_id)
+                            .to_set
+
+    @stash_by_brand_count = @stashed_color_ids.count
 
     @colors_by_family = ProductColor::COLOR_FAMILIES.filter_map do |family|
       colors = @product_colors.by_family(family).to_a
