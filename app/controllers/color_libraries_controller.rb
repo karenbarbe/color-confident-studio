76b4cd33bf48c23737636c3969432fd0d867bbdb
Karen Barbé <info@karenbarbe.com>
2025-12-02 11:13:16 -0600

    refactor color libraries index view and add spectrum sample method to brand model

--- app/controllers/color_libraries_controller.rb
+++ app/controllers/color_libraries_controller.rb
@@ -3,9 +3,9 @@ class ColorLibrariesController < ApplicationController
   def index
     @categories = Brand.categories.keys
     @brands_by_category = Brand.all.group_by(&:category)
-    @featured = Brand.main.first
-    @featured_colors = @featured.product_colors.order(:id).first(20)
-    @stashed_color_ids = Current.user.stash_items.pluck(:product_color_id).to_set
+    # @featured = Brand.main.first
+    # @featured_colors = @featured.product_colors.order(:id).first(20)
+    # @stashed_color_ids = Current.user.stash_items.pluck(:product_color_id).to_set
   end
 
   def category
